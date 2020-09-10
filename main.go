893797d21e79379f1017b1ea50d98fc49afd0918
Robin Kaiser <t@r-k.mx>
2020-09-10 20:35:10 +0200

    upgrades, people, upgrades

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"github.com/xanzy/go-gitlab"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 	"time"
 )
 
@@ -16,18 +21,16 @@ var (
 	host		string
 	port		string
 	hostport    string
-	repoUrl     string
+	repoName    string
 	gitlabToken string
-	filename    string
 	interval    int
 )
 
 func main() {
 	flag.StringVar(&nmapPath, "nmap", "nmap", "The path to the nmap executable (can be left blank on default, if gloablly available)")
-	flag.StringVar(&hostport, "host", "codeforge.me:443", "The host with port you want to scan")
-	flag.StringVar(&repoUrl, "repo-url", "https://gitlab.com", "The URL of the Git repository you want to use")
-	flag.StringVar(&gitlabToken, "gitlab-token", "123abc", "The token to authenticate with Gitlab")
-	flag.StringVar(&filename, "filename", "report.txt", "The name of the file the report is read from and written back to")
+	flag.StringVar(&hostport, "host", "localhost:80", "The host with port you want to scan")
+	flag.StringVar(&repoName, "reponame", "vendor/repo", "The full name of the Git repository you want to use")
+	flag.StringVar(&gitlabToken, "gitlabtoken", "123abc", "The token to authenticate with Gitlab")
 	flag.IntVar(&interval, "interval", 24, "The interval in hours the check should be run")
 	flag.Parse()
 
@@ -48,7 +51,7 @@ func main() {
 	signal.Notify(quit, os.Interrupt)
 	go func() {
 		<-quit
-		log.Println("quitting")
+		log.Println("exit")
 		// cleanup stuff here
 		close(done)
 	}()
@@ -66,10 +69,82 @@ func main() {
 }
 
 func checkHost() {
+	// 1. execute nmap to get the list of currently supported ciphers
 	c := exec.Command(nmapPath, "-sV", "--script", "ssl-enum-ciphers", "-p", port, host)
 	log.Println("Running command", c.String())
-	o, _ := c.Output()
+	o, err := c.Output()
+	if err != nil {
+		log.Println("coud not get nmap output:", err.Error())
+		return
+	}
+	localReportContent := []byte(getCipherList(strings.ReplaceAll(string(o), "\r", "")))
 
-	log.Println("Result: ", string(o))
-	// do the important stuff here
+
+	// 2. hash the result
+	hasher1 := sha256.New()
+	hasher1.Write(localReportContent)
+	hash1 := hasher1.Sum(nil) // we need that later on
+
+	// 2.5 create client to interact with gitlab repository
+	gitClient, err := gitlab.NewClient(gitlabToken)
+	if err != nil {
+		log.Println("could not establish connection to gitlab with given token", err.Error())
+		return
+	}
+
+	// 3. get remote report file
+	gf := &gitlab.GetFileOptions{
+		Ref: gitlab.String("master"),
+	}
+	remoteFileContent, _, err := gitClient.RepositoryFiles.GetFile(repoName, host + "-report.txt", gf)
+	if err != nil {
+		log.Println("could not get remote file:", err)
+		cf := &gitlab.CreateFileOptions{
+			Branch:        gitlab.String("master"),
+			Content:       gitlab.String(string(o)),
+			CommitMessage: gitlab.String("Adding report file " + host + "-report.txt"),
+		}
+		_, _, err := gitClient.RepositoryFiles.CreateFile(repoName, host + "-report.txt", cf)
+		if err != nil {
+			log.Fatal("could not create file:", err)
+		}
+	}
+	remoteFileContentDecoded, err := base64.StdEncoding.DecodeString(remoteFileContent.Content)
+	if err != nil {
+		log.Println("could not decode base64 content:", err.Error())
+	}
+	ioutil.WriteFile("C:\\Local\\remoteFileContentDecoded.txt", remoteFileContentDecoded, 0777)
+
+	remoteReportContent := []byte(getCipherList(strings.ReplaceAll(string(remoteFileContentDecoded), "\r", "")))
+
+	hasher2 := sha256.New()
+	hasher2.Write(remoteReportContent)
+	hash2 := hasher2.Sum(nil)
+
+	if string(localReportContent) != "" && string(hash1) == string(hash2) {
+		log.Println("report contents matching!")
+	} else {
+		log.Println("report contents NOT matching!")
+		ioutil.WriteFile("C:\\Local\\content1.txt", localReportContent, 0777)
+		ioutil.WriteFile("C:\\Local\\content2.txt", remoteReportContent, 0777)
+	}
+}
+
+func getCipherList(reportContent string) string {
+	l := strings.Split(reportContent, "\n\r\n")
+	//log.Println(l)
+	if len(l) > 1 {
+		log.Println("There is more than 1 block")
+		return l[1]
+	}
+
+	l2 := strings.Split(reportContent, "\n\n")
+	if len(l2) > 1 {
+		log.Println("There is more than 1 block (2)")
+		return l2[1]
+	}
+
+	log.Println("There is not more than 1 block")
+	log.Println(reportContent)
+	return ""
 }
